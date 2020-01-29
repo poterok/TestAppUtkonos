7cd8e3633d81b29cfc442d5c422ebfcae6560e11
Dimov <poterok@gmil.com>
2020-01-29 11:04:03 +0300

    1
    2

---
+++ Test.py
@@ -0,0 +1,7 @@
+
+from appium import webdriver
+
+
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
---
+++ conftest.py
@@ -0,0 +1,6 @@
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '10.0'
+desired_caps['deviceName'] = 'Android'
+desired_caps['appPackage'] = 'ru.utkonos.android.utkonoid'
+desired_caps['appActivity'] = 'ru.utkonos.for_customers.online_store.presentation.activity.splash_screen.SplashScreenActivity'
